dfb0a08328d79650e9eb8282aff86cc15e73611d
Web Developer Sivasundararaj <siva.avis47@gmail.com>
2025-09-06 16:55:20 +0530

    Add files via upload

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+function Footer() {
+  return (
+   <>
+   <div className="flex justify-center p-2 border1 shadow mt-5 pt-5 rounded-xl">
+   <p>@Silpautomech-Peramblur </p>
+   </div>
+   </>
+  )
+  }
+  export default Footer
