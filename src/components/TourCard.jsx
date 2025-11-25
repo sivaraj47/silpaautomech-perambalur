df9cac40a850e3275cd76b766f48b08bb7bb55a5
Web Developer Sivasundararaj <siva.avis47@gmail.com>
2025-11-25 15:57:12 +0530

    Update TourCard.jsx

--- src/components/TourCard.jsx
+++ src/components/TourCard.jsx
@@ -21,8 +21,8 @@ function TourCard({ img, titles, description, isFullWidth,url}) {
 	${isFullWidth ? "h-96 bg-top" : " h-48 max-md:h-96"}`} />
 	
       <div className="p-2 font-roboto text-center">
-        <div   onClick={() => window.open(url, "_blank")}
-		 className="text-accent font-semibold text-[#222] bg-yellow-500 text-[14px] p-2  w-48 m-auto border-1 rounded-xl mb-2">{titles}</div>
+        <a   onClick={() => window.open(url, "_blank")}
+		 className="text-accent font-semibold text-[#222] bg-yellow-500 text-[14px] p-2  w-48 m-auto border-1 rounded-lg mb-2" href="#">{titles}</a>
         <p className="font-bold text-[14px] text-par mb-2">{description}</p>
       </div>
 	</div>
@@ -32,6 +32,7 @@ function TourCard({ img, titles, description, isFullWidth,url}) {
 }
 
 export default TourCard
+
 
 
 
