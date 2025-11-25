601995271afdde94ca6722ff88563b6147ca0815
Web Developer Sivasundararaj <siva.avis47@gmail.com>
2025-11-25 12:19:55 +0530

    Update NavHeader.jsx

--- src/components/NavHeader.jsx
+++ src/components/NavHeader.jsx
@@ -35,9 +35,9 @@ function NavHeader({mtitle,stitle,stitle2,stitle3,smtit,profile,insta}) {
 	
 <div className="flex flex-col1 justify-center items-center my-2" id="about">
 	<div className="flex justify-center w-[50%] flex-col items-center mt-5">
-		<img className=" w-[85%] h-100 object-cover  rounded-2xl border1 1bg-gray-200 shadow-xl shadow-gray-500/50 mt-2" src="https://piaggio-cv.co.in/wp-content/themes/piaggio/assets/img/product/passenger/auto-plus/Yellow_45454+Door.png">
+		<img className=" w-[85%] h-100 object-cover  rounded-2xl border1 1bg-gray-200 shadow-xl shadow-gray-500/50 mt-2" src="https://piaggio-cv.co.in/wp-content/themes/piaggio/assets/img/product/passenger/auto-plus/Yellow_45454+Door.png"/>
 			<p className="text-xl p-2 max-md:text-xl">Saravanasankar - sales executive</p>
-			<img className="" src="https://img.icons8.com/fluency/32/instagram-new.png">
+			<img className="" src="https://img.icons8.com/fluency/32/instagram-new.png"/>
 			</div>
 			<div className="flex justify-arounded w-[50%] items-center w-full1 max-md:flex-col">
 				<div className="w-full1 text-left">
@@ -61,5 +61,6 @@ function NavHeader({mtitle,stitle,stitle2,stitle3,smtit,profile,insta}) {
 	
 }
 export default NavHeader
+
 
 
