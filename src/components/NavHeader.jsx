30fdac6026ba421fbafad43a32cb7656e3901e11
Web Developer Sivasundararaj <siva.avis47@gmail.com>
2025-11-25 12:32:14 +0530

    Update NavHeader.jsx

--- src/components/NavHeader.jsx
+++ src/components/NavHeader.jsx
@@ -36,7 +36,7 @@ function NavHeader({mtitle,stitle,stitle2,stitle3,smtit,profile,insta}) {
 <div className="flex flex-col1 justify-center items-center my-2" id="about">
 	<div className="flex justify-center w-[50%] flex-col items-center mt-5">
 		<img className=" w-[85%] h-100 object-cover  rounded-2xl border1 1bg-gray-200 shadow-xl shadow-gray-500/50 mt-2" src="https://piaggio-cv.co.in/wp-content/themes/piaggio/assets/img/product/passenger/auto-plus/Yellow_45454+Door.png"/>
-			<p className="text-xl p-2 max-md:text-xl">Saravanasankar - sales executive</p>
+			
 			<img className="" src="https://img.icons8.com/fluency/32/instagram-new.png"/>
 			</div>
 			<div className="flex justify-arounded w-[50%] items-center w-full1 max-md:flex-col">
@@ -44,6 +44,7 @@ function NavHeader({mtitle,stitle,stitle2,stitle3,smtit,profile,insta}) {
 					<p className="text-2xl p-2 max-md:text-xl">Perambalur, Ariyalur, Thuraiyur, and Tholudur</p>
 					<p className="text-[35px] max-md:text-2xl p-2">Move Anywhere, More Easily Than Ever - Ape Auto</p>
 					<div className="list-inside1 list-disc1 mb-6 flex flex-wrap">
+						<p className="text-xl p-2 max-md:text-xl mb-2">Saravanasankar - sales executive</p>
 						<div className="text-sm p-2 max-md:text-xl w-[50%]"> 15+ years of experience .</div>
 						<div className="text-sm p-2 max-md:text-xl w-[50%]"> trusted service, .</div>
 						<div className="text-sm p-2 max-md:text-xl w-[50%]">5000+ serve customers</div>
@@ -61,6 +62,7 @@ function NavHeader({mtitle,stitle,stitle2,stitle3,smtit,profile,insta}) {
 	
 }
 export default NavHeader
+
 
 
 
